d3020eaedcbc814de75c1b71df3f403bf1625643
gomes-tkd <jgomestkd@gmail.com>
2024-08-17 13:38:26 -0300

    Chess layer and printing the board

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import bordgame.Board;
 import bordgame.Position;
+import chess.ChessMatch;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ChessMatch chessMatch = new ChessMatch();
 
-
+        UI.printBoard(chessMatch.getPieces());
     }
 }
