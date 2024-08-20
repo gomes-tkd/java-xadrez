fa06e25c8a75ba4b07be8d6bd888e1ab9444f9f7
gomes-tkd <jgomestkd@gmail.com>
2024-08-20 00:27:13 -0300

    Moving pieces

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
 import bordgame.Board;
 import bordgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
-        UI.printBoard(chessMatch.getPieces());
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+
+            System.out.println();
+            System.out.println("Source: ");
+            ChessPosition source = UI.readChessPosition(scanner);
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = UI.readChessPosition(scanner);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+        }
+
     }
 }
