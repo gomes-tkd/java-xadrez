7c7e0fb6b884192cfd7c347fcb638562d44a57ab
gomes-tkd <jgomestkd@gmail.com>
2024-08-20 09:43:14 -0300

    Handling exceptions and clearing screen

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import bordgame.Board;
 import bordgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -14,16 +16,22 @@ public class Main {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            UI.printBoard(chessMatch.getPieces());
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
 
-            System.out.println();
-            System.out.println("Source: ");
-            ChessPosition source = UI.readChessPosition(scanner);
-            System.out.println();
-            System.out.println("Target: ");
-            ChessPosition target = UI.readChessPosition(scanner);
+                System.out.println();
+                System.out.println("Source: ");
+                ChessPosition source = UI.readChessPosition(scanner);
+                System.out.println();
+                System.out.println("Target: ");
+                ChessPosition target = UI.readChessPosition(scanner);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException | InputMismatchException e) {
+                System.out.println(e.getMessage());
+                scanner.nextLine();
+            }
 
         }
 
